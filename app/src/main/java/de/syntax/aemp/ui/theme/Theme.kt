6ec52ce37b2eac71ebe52a44bd818e39ead1857e
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-06-16 15:33:21 +0200

    schrift eingefügt

--- app/src/main/java/de/syntax/aemp/ui/theme/Theme.kt
+++ app/src/main/java/de/syntax/aemp/ui/theme/Theme.kt
@@ -1,5 +1,5 @@
-package com.example.compose
-import android.app.Activity
+package de.syntax.aemp.ui.theme
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -7,12 +7,78 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import com.example.compose.backgroundDark
+import com.example.compose.backgroundLight
+import com.example.compose.errorContainerDark
+import com.example.compose.errorContainerLight
+import com.example.compose.errorDark
+import com.example.compose.errorLight
+import com.example.compose.inverseOnSurfaceDark
+import com.example.compose.inverseOnSurfaceLight
+import com.example.compose.inversePrimaryDark
+import com.example.compose.inversePrimaryLight
+import com.example.compose.inverseSurfaceDark
+import com.example.compose.inverseSurfaceLight
+import com.example.compose.onBackgroundDark
+import com.example.compose.onBackgroundLight
+import com.example.compose.onErrorContainerDark
+import com.example.compose.onErrorContainerLight
+import com.example.compose.onErrorDark
+import com.example.compose.onErrorLight
+import com.example.compose.onPrimaryContainerDark
+import com.example.compose.onPrimaryContainerLight
+import com.example.compose.onPrimaryDark
+import com.example.compose.onPrimaryLight
+import com.example.compose.onSecondaryContainerDark
+import com.example.compose.onSecondaryContainerLight
+import com.example.compose.onSecondaryDark
+import com.example.compose.onSecondaryLight
+import com.example.compose.onSurfaceDark
+import com.example.compose.onSurfaceLight
+import com.example.compose.onSurfaceVariantDark
+import com.example.compose.onSurfaceVariantLight
+import com.example.compose.onTertiaryContainerDark
+import com.example.compose.onTertiaryContainerLight
+import com.example.compose.onTertiaryDark
+import com.example.compose.onTertiaryLight
+import com.example.compose.outlineDark
+import com.example.compose.outlineLight
+import com.example.compose.outlineVariantDark
+import com.example.compose.outlineVariantLight
+import com.example.compose.primaryContainerDark
+import com.example.compose.primaryContainerLight
+import com.example.compose.primaryDark
+import com.example.compose.primaryLight
+import com.example.compose.scrimDark
+import com.example.compose.scrimLight
+import com.example.compose.secondaryContainerDark
+import com.example.compose.secondaryContainerLight
+import com.example.compose.secondaryDark
+import com.example.compose.secondaryLight
+import com.example.compose.surfaceBrightDark
+import com.example.compose.surfaceBrightLight
+import com.example.compose.surfaceContainerDark
+import com.example.compose.surfaceContainerHighDark
+import com.example.compose.surfaceContainerHighLight
+import com.example.compose.surfaceContainerHighestDark
+import com.example.compose.surfaceContainerHighestLight
+import com.example.compose.surfaceContainerLight
+import com.example.compose.surfaceContainerLowDark
+import com.example.compose.surfaceContainerLowLight
+import com.example.compose.surfaceContainerLowestDark
+import com.example.compose.surfaceContainerLowestLight
+import com.example.compose.surfaceDark
+import com.example.compose.surfaceDimDark
+import com.example.compose.surfaceDimLight
+import com.example.compose.surfaceLight
+import com.example.compose.surfaceVariantDark
+import com.example.compose.surfaceVariantLight
+import com.example.compose.tertiaryContainerDark
+import com.example.compose.tertiaryContainerLight
+import com.example.compose.tertiaryDark
+import com.example.compose.tertiaryLight
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -92,7 +158,7 @@ private val darkScheme = darkColorScheme(
 
 
 @Composable
-fun AppTheme(
+fun AEMPTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
@@ -110,7 +176,8 @@ fun AppTheme(
 
   MaterialTheme(
     colorScheme = colorScheme,
-      content = content
+    typography = Typography,
+    content = content
   )
 }
 
--- app/src/main/java/de/syntax/aemp/ui/theme/Type.kt
+++ app/src/main/java/de/syntax/aemp/ui/theme/Type.kt
@@ -2,33 +2,35 @@ package de.syntax.aemp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import de.syntax.aemp.R
 
+val adlamdisplayFontFamily = FontFamily(
+    Font(R.font.adlamdisplay_regular)
+)
+
+val baseLine = Typography()
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = baseLine.displayLarge.copy(fontFamily = adlamdisplayFontFamily),
+    displayMedium = baseLine.displayMedium.copy(fontFamily = adlamdisplayFontFamily),
+    displaySmall = baseLine.displaySmall.copy(fontFamily = adlamdisplayFontFamily),
+    headlineLarge = baseLine.headlineLarge.copy(fontFamily = adlamdisplayFontFamily),
+    headlineMedium = baseLine.headlineMedium.copy(fontFamily = adlamdisplayFontFamily),
+    headlineSmall = baseLine.headlineSmall.copy(fontFamily = adlamdisplayFontFamily),
+    titleLarge = baseLine.titleLarge.copy(fontFamily = adlamdisplayFontFamily),
+    titleMedium = baseLine.titleMedium.copy(fontFamily = adlamdisplayFontFamily),
+    titleSmall = baseLine.titleSmall.copy(fontFamily = adlamdisplayFontFamily),
+    bodyLarge = baseLine.bodyLarge.copy(fontFamily = adlamdisplayFontFamily),
+    bodyMedium = baseLine.bodyMedium.copy(fontFamily = adlamdisplayFontFamily),
+    bodySmall = baseLine.bodySmall.copy(fontFamily = adlamdisplayFontFamily),
+    labelLarge = baseLine.labelLarge.copy(fontFamily = adlamdisplayFontFamily),
+    labelMedium = baseLine.labelMedium.copy(fontFamily = adlamdisplayFontFamily),
+    labelSmall = baseLine.labelSmall.copy(fontFamily = adlamdisplayFontFamily),
 )
+
+
+
