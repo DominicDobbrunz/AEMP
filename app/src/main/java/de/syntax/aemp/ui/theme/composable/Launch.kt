b5d404f6e6294c3ccdaf91eca9c2c74a526d8f48
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-06-16 15:57:32 +0200

    Launch erstellt

---
+++ app/src/main/java/de/syntax/aemp/ui/theme/composable/Launch.kt
@@ -0,0 +1,33 @@
+package de.syntax.aemp.ui.theme.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import de.syntax.aemp.R
+
+
+@Composable
+internal fun Launch() {
+
+    Box {
+
+        Image(
+            painter = painterResource(R.drawable.medi),
+            contentDescription = "Logo",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LaunchPreviw() {
+    Launch()
+}
