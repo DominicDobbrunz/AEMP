78ff4fbb99895e5440f6faa1d63b19c6473020cc
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-06-16 13:22:47 +0200

    theme eingefügt

--- app/src/main/java/de/syntax/aemp/MainActivity.kt
+++ app/src/main/java/de/syntax/aemp/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import de.syntax.aemp.ui.theme.AEMPTheme
+import com.example.compose.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AEMPTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "AEMP",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AEMPTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
