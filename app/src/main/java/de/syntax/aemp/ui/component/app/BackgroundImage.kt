1742a03aedd0add88f24da37ec9e027f2f0ad6e1
Dominic Dobbrunz <d.dobbrunz@gmx.de>
2025-07-21 10:37:44 +0200

    image bearbeitet

--- app/src/main/java/de/syntax/aemp/ui/component/app/BackgroundImage.kt
+++ app/src/main/java/de/syntax/aemp/ui/component/app/BackgroundImage.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,8 +15,7 @@ fun BackgroundImage() {
             painter = painterResource(id = R.drawable.medi2),
             contentDescription = "Hintergrund",
             modifier = Modifier
-                .fillMaxSize()
-                .alpha(0.9f),
+                .fillMaxSize(),
             contentScale = ContentScale.Crop
         )
 }
